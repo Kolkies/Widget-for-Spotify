62bfb23412a94d73dbb522945ff84f161ead95aa
Netlob <sjoerdabolten@gmail.com>
2020-09-23 23:26:04 +0200

    Initial commit

---
+++ Widget for Spotify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Widget for Spotify
+//
+//  Created by Sjoerd Bolten on 23/09/2020.
+//  Copyright © 2020 Sjoerd Bolten. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
